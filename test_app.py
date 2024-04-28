511ded85d0b1856def0e596c6ea25898efbf61e1
Ilyas Shaikh <72152969+ilyas829@users.noreply.github.com>
2024-04-28 19:56:41 +0530

    Creating the Assignment Repository
    In this commit all the files for the flask app are created

---
+++ test_app.py
@@ -0,0 +1,29 @@
+# test_app.py
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_route_0(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_hello_route_1(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+
+    def test_home_route_0(self):
+        response = self.app.get('/home')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_hello_route_1(self):
+        response = self.app.get('/home', follow_redirects=True)
+        self.assertIn(b'Welcome to My Home Page', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
